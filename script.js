fe6689345b3a554df7a962f082ed741854b7b8d8
João Victor <joaovictordossantos290@gmail.com>
2025-11-09 19:56:23 -0300

    versao inicial (v1.0.0)

---
+++ script.js
@@ -0,0 +1,20 @@
+function converter() {
+    let valor = parseFloat(document.getElementById("valor").value);
+    let de = document.getElementById("de").value;
+    let para = document.getElementById("para").value;
+
+    if (isNaN(valor)) {
+        document.getElementById("resultado").innerText = "Digite um valor válido!";
+        return;
+    }
+
+    // Converte "de" para Joules (J)
+    if (de === "cal") valor *= 4.184;
+    if (de === "kwh") valor *= 3600000;
+
+    // Converte de Joules para a unidade final
+    if (para === "cal") valor /= 4.184;
+    if (para === "kwh") valor /= 3600000;
+
+    document.getElementById("resultado").innerText = "Resultado: " + valor;
+}
